789006420f282bc7fd7073758ba4afe5943bf326
VINCENT Thomas <losdingtro@gmail.com>
2019-10-15 08:52:13 +0200

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,11 @@
+# Buyify
+
+## Installation
+
+To install this repository, clone it then run `npm install`
+It may also be necessary to run `npm run build`
+
+## Structure
+
+All of the website is stored in the `public` repository.
+The sass and the framework as stored in the `src/sass` and `framework` directories respectively.
---
+++ public/model/DAO.class.php
@@ -0,0 +1,35 @@
+<?php
+
+    class DAO {
+        private $db;
+        private $database = 'sqlite:../data/buyify.db';
+
+        function __construct() {
+            try {
+              $this->db = new PDO($this->database);
+            }
+            catch (PDOException $e){
+              die("erreur de connexion:".$e->getMessage());
+            }
+        }
+
+        function select(string $table, string $where = '1', $bind = [], string $fields = '*') : array {
+            $req = $this->db->prepare("SELECT $fields FROM $table WHERE $where");
+            $req->execute($bind);
+            return $req->fetchAll();
+        }
+
+        function selectAsClass(string $class, string $table, string $where = '1', $bind = [], string $fields = '*') : array {
+            $req = $this->db->prepare("SELECT $fields FROM $table WHERE $where");
+            $req->execute($bind);
+            return $req->fetchAll(PDO::FETCH_CLASS, $class);
+        }
+
+        function run($sql, $bind = []) {
+            $req = $this->db->prepare($sql);
+            $req->execute($bind);
+            return $req->fetchAll();
+        }
+    }
+
+    ?>
