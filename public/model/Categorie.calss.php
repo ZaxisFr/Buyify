c447989c87f156807b5599fea0f71ca128472332
ZaxisFr <nilskrattinger@yahoo.fr>
2019-10-22 08:45:27 +0200

    feature-catégorie : view

--- public/model/Categorie.calss.php
+++ public/model/Categorie.calss.php
@@ -13,13 +13,12 @@ class Categorie
      * @throws Exception si la valeur de $nom est null
      */
     public function __construct(string $nom, Categorie $parent=null) {
-        try{
-            $this->setNom($nom);
-            $this->setParent($parent);
-        } catch (Exception $e){
-            throw $e;
+        if(is_null($nom)){
+            throw new Exception('Nom de catégoire vide');
+        } else {
+            $this->_nom = $nom;
         }
-
+        $this->_parent = $parent;
     }
 
     /**
@@ -28,32 +27,11 @@ class Categorie
     public function getNom(): string {
         return $this->_nom;
     }
-
-    /**
-     * @param string $nom
-     * @throws Exception si $nom est null
-     */
-    public function setNom(string $nom): void {
-        if(is_null($nom)){
-            throw new Exception('Nom de catégoire vide');
-        } else {
-            $this->_nom = $nom;
-        }
-        
-    }
-
-    /**
+        /**
      * @return Categorie
      */
     public function getParent(): Categorie {
         return $this->_parent;
-    }
-
-    /**
-     * @param Categorie $parent
-     */
-    public function setParent(Categorie $parent): void {
-        $this->_parent = $parent;
     }
 
     /**
