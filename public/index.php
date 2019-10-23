e46dcc49d0e273002e156dcb6898a67591bfdde1
ZaxisFr <nilskrattinger@yahoo.fr>
2019-10-23 17:44:45 +0200

    front-gestion-categorie : supression des donc inutiles

--- public/index.php
+++ public/index.php
@@ -1,3 +1,2 @@
 <a href="controller/inscription.ctrl.php">Inscritpion</a><br>
 <a href="controller/categorieCreation.ctrl.php">Categorie</a><br>
-<a href="controller/categorieSuppression.ctrl.php">Supression</a><br>
