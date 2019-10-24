05a92428967dbcae79811807408964124bf66025
ZaxisFr <nilskrattinger@yahoo.fr>
2019-10-24 17:04:58 +0200

    front-gestion-categorie : Correction des fautes et fix de : https://github.com/FeelZoR/Buyify/pull/6#discussion_r338520487 https://github.com/FeelZoR/Buyify/pull/6#discussion_r338520877

--- public/view/categorieCreation.view.php
+++ public/view/categorieCreation.view.php
@@ -2,7 +2,7 @@
     <p class="bg-danger banner" xmlns="http://www.w3.org/1999/html"><?= $erreur ?> <button type="button" class="close" aria-label="close"><span aria-hidden="true">&times;</span></button></p>
 <?php endif; ?>
 <?php if (isset($succes)): ?>
-    <p class="bg-success banner">Operation effectuée <button type="button" class="close" aria-label="close"><span aria-hidden="true">&times;</span></button></p>
+    <p class="bg-success banner">Opération effectuée <button type="button" class="close" aria-label="close"><span aria-hidden="true">&times;</span></button></p>
 <?php endif;?>
 
 <div id="categorie-body" class="container_fluid">
@@ -13,16 +13,15 @@
         <div class="card-body mx-3">
             <form action="<?= $_SERVER['PHP_SELF']?>" method="post" >
                 <div class="form-group">
-                    <label for="cat_name">Nom de la catégorie </label>
-                    <input  class="form-control" type="text" name="cat_name" id="cat_name" required> </br>
+                    <label for="cat-name">Nom de la catégorie </label>
+                    <input  class="form-control" type="text" name="cat_name" id="cat-name" required> </br>
                 </div>
                 <div class="form-group">
-                    <label for="cat_name">Catégorie parente</label>
+                    <label for="parent">Catégorie parente</label>
                     <select  class="form-control" id="parent" name="parent" required>
-                        <?php foreach ($cats as $cat){
-                            echo "<option value=\"" . $cat['nom'] . "\">" . $cat['nom'] . "</option>";
-                        }
-                        ?>
+                        <?php foreach ($cats as $cat){ ?>
+                        <option value="<?= $cat['nom'] ?>"><?= $cat['nom'] ?></option>
+                        <?php }; ?>
                     </select></br>
                 </div>
         </div>
@@ -41,12 +40,14 @@
                 <div class="form-group">
                     <label for="cat">Nom de la catégorie </label>
                     <select id="cat" class="form-control" name="cat" required>
-                        <?php foreach ($cats as $cat){echo "<option value=\"" . $cat['nom'] . "\">" . $cat['nom'] . "</option>";} ?>
+                        <?php foreach ($cats as $cat){ ?>
+                            <option value="<?= $cat['nom'] ?>"><?= $cat['nom'] ?></option>
+                        <?php }; ?>
                     </select></br>
                 </div>
         </div>
         <div class="card-footer text-muted">
-            <button type="button" class="btn btn-primary btn-block" data-toggle="modal" data-target="#exampleModal">
+            <button type="button" class="btn btn-primary btn-block" data-toggle="modal" data-target="#confirmation-popup">
                 Supprimer
             </button>
         </div>
@@ -56,20 +57,20 @@
 
 
 
-<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal fade" id="confirmation-popup" tabindex="-1" role="dialog" aria-labelledby="confirmation-popup" aria-hidden="true">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="ModalLabel">Confirmer la supression</h5>
-                   
+                <h5 class="modal-title" id="moda-lLabel">Confirmer la suppression</h5>
                 </button>
             </div>
             <div class="modal-body">
-                <h5>ATTENTION !! </h5> </br> La suppression est définitve vous ne pourrez pas l'annulez
+                <h5>ATTENTION !! </h5>
+                <p>La suppression est définitive, les données ne seront pas récupérables.</p>
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-dismiss="modal">Annuler</button>
-                <button form="supp" class="btn btn-primary btn-block" type="submit" value="Supprimer" name="supprimerCat" /> Confimer </button>
+                <button form="supp" class="btn btn-primary btn-block" type="submit" value="Supprimer" name="supprimerCat" /> Confirmer </button>
             </div>
         </div>
     </div>
