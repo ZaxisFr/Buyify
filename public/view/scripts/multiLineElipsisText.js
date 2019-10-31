16080bd02b805b6a23b5f18b32f53f4b6c338df0
Feelzor <losdingtro@gmail.com>
2019-10-31 14:39:21 +0100

    feature-favorie: fix js style

--- public/view/scripts/multiLineElipsisText.js
+++ public/view/scripts/multiLineElipsisText.js
@@ -1,7 +1,7 @@
 function multiLineElipsisText(){
     const containers = document.querySelectorAll('.text-multiline-elipsis');
     Array.prototype.forEach.call(containers, (container) => {  // Loop through each container
-        var divh = container.clientHeight;
+        const divh = container.clientHeight;
         container.textContent = container.getAttribute("backupContent");
         while (container.scrollHeight-5> divh) { // Check if the paragraph's height is taller than the container's height. If it is:
             container.textContent = container.textContent.replace(/\W*\s(\S)*$/, '...'); // add an ellipsis at the last shown space
@@ -12,8 +12,8 @@ function setupLineElipsisText(){
     const containers = document.querySelectorAll('.text-multiline-elipsis');
     Array.prototype.forEach.call(containers, (container) => {  // Loop through each container
         container.setAttribute("backupContent",container.textContent);
-    })
+    });
 }
 setupLineElipsisText();
 multiLineElipsisText();
-window.addEventListener('resize',multiLineElipsisText,true)
+window.addEventListener('resize',multiLineElipsisText,true);
