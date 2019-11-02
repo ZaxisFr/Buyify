4638e9bc407703cbd6b035b53981ed8612a33e30
Feelzor <losdingtro@gmail.com>
2019-11-02 17:46:55 +0100

    feature-page-accueil: ajout de la page d'accueil

--- public/index.php
+++ public/index.php
@@ -1,6 +1,1 @@
-<a href="controller/inscription.ctrl.php">Inscritpion</a><br>
-<a href="controller/categorieCreation.ctrl.php">Categorie</a><br>
-<a href="controller/produitMain.ctrl.php">Produits</a><br>
-<a href="controller/produitAjout.ctrl.php">ProduitsAjout</a><br>
-<a href="controller/message.ctrl.php">Messages</a><br>
-<a href="controller/favori.ctrl.php">Favori</a><br>
+<?php header('Location: controller/produitMain.ctrl.php');
