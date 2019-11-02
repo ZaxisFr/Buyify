f9f0d941dcd3a435988db16c7e2a0574bcdfa4b0
Feelzor <losdingtro@gmail.com>
2019-11-02 16:30:35 +0100

    feature-affichage-produits: fix orthographe

--- public/controller/produit.ctrl.php
+++ public/controller/produit.ctrl.php
@@ -16,7 +16,7 @@ $view = new View();
 if(isset($_GET['id'])){
     $view->assign('produit',getProduit($_GET['id']));
 } else {
-    header('Location: errorPage.ctrl.php?error=400&msg="Id de produit recherché manquant ou incorrecte"');
+    header('Location: errorPage.ctrl.php?error=400&msg="Id de produit recherché manquant ou incorrect"');
     exit(0);
 }
 
