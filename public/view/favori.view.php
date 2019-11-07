e45697dbc383b267a7b1cb25d3f0e1254dee2d6d
ZaxisFr <nilskrattinger@yahoo.fr>
2019-11-07 12:03:01 +0100

    fix-favorie-vide-style: Ajout message fav vide + uniformisation des pages de prdouits

--- public/view/favori.view.php
+++ public/view/favori.view.php
@@ -9,5 +9,11 @@
             }
         ?>
     </div>
+    <?php if(sizeof($this->param['produitsFavoris']) == 0): ?>
+        <article class="fav-erreur">
+            <h3>Humm ça à l'air bien vide par ici...</h3>
+            <p>vous pouvez ajouter un produit en favori en cliant sur la petite étoile située en haut à doite d'un produit</p>
+        </article>
+    <?php endif; ?>
     <script type="text/javascript" src="../view/scripts/multiLineElipsisText.js"></script>
 </div>
